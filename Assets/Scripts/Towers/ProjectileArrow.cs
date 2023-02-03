cad4039b59e91e3f4413cc79d74169247c43b402
Szabó-Vukov Levente <szabo.vukov.levente@gmail.com>
2023-02-03 20:37:19 +0100

    arrow angle fix

--- Assets/Scripts/Towers/ProjectileArrow.cs
+++ Assets/Scripts/Towers/ProjectileArrow.cs
@@ -6,13 +6,19 @@ public class ProjectileArrow : MonoBehaviour
 {
     public Transform pfProjectileArrow;
     private float moveSpeed = 8f;
-    private float destroyDistance = 1f;
-    private Vector3 targetPositionLocale;
+    private float destroyDistance = 0.2f;
+    public Vector3 targetPositionLocale;
 
     // Start is called before the first frame update
     void Start()
     {
 
+    }
+
+    public void Create(Vector3 spawnPosition, Vector3 targetPosition)
+    {
+        Instantiate(pfProjectileArrow, spawnPosition, Quaternion.identity);
+        targetPositionLocale = targetPosition;
     }
 
     // Update is called once per frame
@@ -22,7 +28,7 @@ public class ProjectileArrow : MonoBehaviour
         transform.position += moveDir * moveSpeed * Time.deltaTime;
 
         float angle = GetAngleFromVectorFloat(moveDir);
-        transform.eulerAngles = new Vector3(0, 0, angle);
+        transform.eulerAngles = Vector3.forward * angle;
 
         if (Vector3.Distance(targetPositionLocale, transform.position) < destroyDistance)
         {
@@ -30,18 +36,10 @@ public class ProjectileArrow : MonoBehaviour
         }
     }
 
-    public void Create(Vector3 spawnPosition, Vector3 targetPosition)
-    {
-        Instantiate(pfProjectileArrow, spawnPosition, Quaternion.identity);
-        targetPositionLocale = targetPosition;
-        Debug.Log(targetPosition);
-        Debug.Log(targetPositionLocale);
-    }
-
     private float GetAngleFromVectorFloat(Vector3 dir)
     {
         dir = dir.normalized;
-        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90;
         if (angle < 0)
         {
             angle += 360;
