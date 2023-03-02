4d46bf948f907d0c183dbed0c150c21a054ba2fc
Simon Zsombor <simon.zsombor06@gmail.com>
2023-03-02 08:22:34 +0100

    Screen Start

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -17,7 +17,7 @@ public class CameraController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject StartGameUI;
+    public GameObject Pause;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void startGame()
+    {
+        Time.timeScale = 1;
+        StartGameUI.SetActive(false);
+    }
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+        Pause.SetActive(true);
+    }
+    public void ContinueGame()
+    {
+        Time.timeScale = 1;
+        Pause.SetActive(false);
+    }
+    public void ExitGame()
+    {     
+         Application.Quit();   
+    }
+}
